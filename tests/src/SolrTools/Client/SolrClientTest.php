1a7e25e7f256aa9b851b59ecdcf235c5352ed053
Yago Riveiro <yago.riveiro@gmail.com>
2013-10-30 14:43:36 +0000

    Tests: Improved SolrClient test case.

--- tests/src/SolrTools/Client/SolrClientTest.php
+++ tests/src/SolrTools/Client/SolrClientTest.php
@@ -49,6 +49,23 @@ class ClientTest extends PHPUnit_Framework_TestCase
 		);
 	}
 
+	public function testCreateCollectionFailureNodeDown()
+	{
+		$cli = new SolrClient(array('unknown:8983'));
+
+		$parameters = array(
+			'name' => 'phpunit',
+			'numShards' => 2,
+			'replicationFactor' => 1,
+			'maxShardsPerNode' => 2,
+			'collection.configName' => 'default'
+		);
+
+		list($code, $response) = $cli->createCollection($parameters);
+
+		$this->assertEquals(500, $code);
+	}
+
 	public function testDeleteCollection()
 	{
 		$parameters = array(
@@ -75,5 +92,22 @@ class ClientTest extends PHPUnit_Framework_TestCase
 			'Could not find collection:collectionNotExists',
 			$response->error->msg
 		);
+	}
+
+	public function testDeleteCollectionFailureNodeDown()
+	{
+		$cli = new SolrClient(array('unknown:8983'));
+
+		$parameters = array(
+			'name' => 'phpunit',
+			'numShards' => 2,
+			'replicationFactor' => 1,
+			'maxShardsPerNode' => 2,
+			'collection.configName' => 'default'
+		);
+
+		list($code, $response) = $cli->deleteCollection($parameters);
+
+		$this->assertEquals(500, $code);
 	}
 }
