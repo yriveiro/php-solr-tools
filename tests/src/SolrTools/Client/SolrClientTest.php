e30c266a1d63e8e39a570b9eb8c71afb5640ac3a
Yago Riveiro <yago.riveiro@gmail.com>
2013-10-30 15:11:40 +0000

    Tests: Remove useless parameter from testDeleteCollectionFailureNodeDown test case.

--- tests/src/SolrTools/Client/SolrClientTest.php
+++ tests/src/SolrTools/Client/SolrClientTest.php
@@ -100,10 +100,6 @@ class ClientTest extends PHPUnit_Framework_TestCase
 
 		$parameters = array(
 			'name' => 'phpunit',
-			'numShards' => 2,
-			'replicationFactor' => 1,
-			'maxShardsPerNode' => 2,
-			'collection.configName' => 'default'
 		);
 
 		list($code, $response) = $cli->deleteCollection($parameters);
